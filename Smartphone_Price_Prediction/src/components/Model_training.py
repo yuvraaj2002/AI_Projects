998aeea76ea020d4cd9c31f3f8afff498fc875ac
Yuvraj Singh <ys2002github@gmail.com>
2023-07-31 12:02:43 +0530

    Created model training file

--- Smartphone_Price_Prediction/src/components/Model_training.py
+++ Smartphone_Price_Prediction/src/components/Model_training.py
@@ -2,15 +2,17 @@ from src.logger import logging
 from src.exception import CustomException
 import sys
 import os
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import cross_val_score
 from dataclasses import dataclass
 from src.utils import save_object
 
 
 @dataclass
 class Model_training_Config:
-    model_trained_path = os.path.join("artifacts", "model.pkl")
+    model_trained_path = os.path.join("artifacts", "Model.pkl")
 
 
 class Train_Model:
@@ -19,20 +21,9 @@ class Train_Model:
 
     def initialize_model_training(self, X_train, y_train, X_test, y_test):
         try:
-            logging.info("Model training intialized")
+            logging.info("Model training Initialized")
 
-            model = GradientBoostingClassifier(
-                loss="log_loss",
-                learning_rate=0.11818576935196282,
-                n_estimators=322,
-                subsample=0.919894055658876,
-                criterion="friedman_mse",
-                min_samples_split=10,
-                min_samples_leaf=5,
-                min_weight_fraction_leaf=0.020616418092142458,
-                max_depth=9,
-                min_impurity_decrease=0.8342382108843562,
-            )
+            model = RandomForestRegressor()
 
             # Let's train the model
             model.fit(X_train, y_train)
@@ -46,14 +37,11 @@ class Train_Model:
             save_object(file_path=self.model_file_path.model_trained_path, obj=model)
             logging.info("Saved model")
 
-            # Let's calculate accuracy, precision, recall and F1-Score
-            accuracy = accuracy_score(y_test, y_pred)
-            precision = precision_score(y_test, y_pred, average="macro")
-            recall = recall_score(y_test, y_pred, average="macro")
-            f1 = f1_score(y_test, y_pred, average="macro")
-            logging.info("Model training completed")
 
-            return (accuracy, precision, recall, f1)
+            # Let's calculate average R2 score using cross validation
+            scores = cross_val_score(estimator=model, X=X_train, y=y_train, cv=50)
+
+            return (np.mean(scores))
 
         except Exception as e:
             raise CustomException(e, sys)
