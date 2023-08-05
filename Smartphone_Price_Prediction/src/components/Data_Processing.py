b189c2c60608c7cca656a88e4ec4cc8174ea02a9
Yuvraj Singh <ys2002github@gmail.com>
2023-08-05 14:02:25 +0530

    Modified data processing as it will take raw data then split and process

--- Smartphone_Price_Prediction/src/components/Data_Processing.py
+++ Smartphone_Price_Prediction/src/components/Data_Processing.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 import numpy as np
 import re
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
@@ -22,308 +23,37 @@ from src.utils import save_object
 @dataclass
 class Data_Processing_Config:
     pipeline_path = os.path.join("artifacts", "Processing_pipeline.pkl")
+    train_csv_path = os.path.join("artifacts", "train.csv")
+    test_csv_path = os.path.join("artifacts", "test.csv")
 
 
 class Data_processing:
     def __init__(self):
-        self.pipe_path = Data_Processing_Config()
-
-    def find_limits(self,series):
-        """
-        This method will return Upper limit and Lower limit of a series
-        :return:
-        """
-        q1 = series.quantile(0.25)
-        q3 = series.quantile(0.75)
-        IQR = q3 - q1
-        Upper_lmt = q3 + 1.5 * IQR
-        Lower_lmt = q1 - 1.5 * IQR
-        return (Upper_lmt, Lower_lmt)
+        self.paths = Data_Processing_Config()
 
 
-    def extract_encode_sim(self,data):
-        """
-        This method will take data and will return list of pandas series of
-        newly created features with encoded values from the sim feature of that data
-        :param data
-        :return: list of pandas series
-        """
-        # Empty lists to store data
-        Has_5g = []
-        Add_Features = []
-
-        for item in data['sim']:
-            Has_5g.append(1 if '5G,' in item.split() else 0)
-            Add_Features.append(1 if 'NFC,' in item.split() or 'IR,' in item.split(',') else 0)
-
-        Has_5g = pd.Series(Has_5g)
-        Add_Features = pd.Series(Add_Features)
-        return [Has_5g,Add_Features]
-
-    def extract_encode_ram(self,data):
-        """
-        This method will take data and will return list of pandas series of
-        newly created features with encoded values from the sim feature of that data
-        :param data
-        :return: list of pandas series
-        """
-        RAM = []
-        Storage = []
-
-        for item in data['ram']:
-            contents = item.split()
-            if ('RAM,' in contents) or ('inbuilt' in contents):  # Filtered out any wrong info
-
-                # Now we just need to figure out if both are present or only one is present
-                if ('RAM,' in contents) and ('inbuilt' in contents):
-                    RAM.append(int(contents[0]))
-                    Storage.append(int(contents[3]))
-                elif 'RAM,' in contents:
-                    RAM.append(int(contents[0]))
-                    Storage.append(np.nan)
-                elif 'inbuilt' in contents:
-                    RAM.append(np.nan)
-                    Storage.append(int(contents[0]))
-
-            else:
-                RAM.append(np.nan)
-                Storage.append(np.nan)
-
-        RAM = pd.Series(RAM)
-        Storage = pd.Series(Storage)
-        return [RAM,Storage]
-
-
-    def extract_encode_battery(self,data):
-        """
-        This method will take data and will return list of pandas series of
-        newly created features with encoded values from the sim feature of that data
-        :param data
-        :return: list of pandas series
-        """
-        Battery = []
-        Charging = []
-
-        for item in data['battery']:
-            contents = item.split()
-            if ('Battery' in contents) or ('Charging' in contents):  # Filtered out any wrong info
-
-                if ('Battery' in contents) and ('Charging' in contents) and len(contents) == 7:  # Means the W is given
-                    Battery.append(float(contents[0]))
-                    Charging.append(float(contents[4][:-1]))
-
-                # Means keywords are present but value of fast charging is not mentioned
-                elif ('Battery' in contents) and ('Charging' in contents) and len(contents) == 6:
-                    Battery.append(float(contents[0]))
-                    Charging.append(np.nan)
-
-                elif 'Battery' in contents:
-                    Battery.append(float(contents[0]))
-                    Charging.append(np.nan)
-
-            else:
-                Battery.append(np.nan)
-                Charging.append(np.nan)
-
-        Battery = pd.Series(Battery)
-        Charging = pd.Series(Charging)
-        return [Battery,Charging]
-
-
-    def extract_encode_display(self,data):
-        """
-        This method will take data and will return list of pandas series of
-        newly created features with encoded values from the sim feature of that data
-        :param data
-        :return: list of pandas series
-        """
-        PPI = []
-        Screen_RR = []
-
-        for item in data['display']:
-            contents = item.split()
-            if ('inches,' in contents) and ('Display'):  # Filtered out any wrong info
-
-                diagonal_len = float(contents[0])
-                px1_val = float(contents[2])
-                px2_val = float(contents[4])
-                total_px_value = px1_val * px2_val
-                PPI.append(int(total_px_value / diagonal_len))
-            else:
-                PPI.append(np.nan)
-
-        for item in data['display']:
-            contents = item.split()
-            if 'Hz' in contents:
-                Screen_RR.append(int(contents[6]))
-            else:
-                Screen_RR.append(np.nan)
-
-        PPI = pd.Series(PPI)
-        Screen_RR = pd.Series(Screen_RR)
-        return [Screen_RR,PPI]
-
-
-    def extract_encode_camera(self,data):
-        """
-        This method will take data and will return list of pandas series of
-        newly created features with encoded values from the sim feature of that data
-        :param data
-        :return: list of pandas series
-        """
-        rear_cams = []
-        Total_fmp = []
-
-        data['camera'].fillna(data['camera'].mode()[0], inplace=True)
-
-        for item in data['camera']:
-            contents = item.split()
-            if ('Rear' in contents) and ('Front' in contents) and ('&' in contents):  # Filtered out any wrong info
-
-                if 'Quad' in contents:
-                    rear_cams.append(4)
-                elif 'Triple' in contents:
-                    rear_cams.append(3)
-                elif 'Dual' in contents:
-                    rear_cams.append(2)
-                else:
-                    rear_cams.append(1)
-
-                Total_fmp.append(float(item.split('&')[-1].split()[0]))
-
-            else:
-                rear_cams.append(np.nan)
-                Total_fmp.append(np.nan)
-
-        rear_cams = pd.Series(rear_cams)
-        Total_fmp = pd.Series(Total_fmp)
-        return [rear_cams,Total_fmp]
-
-
-    def extract_processor_info(self,data):
-
-        Processor_name = []
-        Processor_core = []
-        Processor_GHz = []
-
-        for item in data['processor']:
-            contents = item.split()
-            if 'Processor' in contents:  # Filtered out wrong info
-
-                if ('Core,' in contents) and ('GHz' in contents):  # When all name,core and Ghz are present
-                    Processor_name.append(contents[0])
-                    Processor_core.append(contents[-5])
-                    Processor_GHz.append(float(contents[-3]))
-
-                elif 'Core,' in contents:  # When only name and core are present
-                    Processor_name.append(contents[0])
-                    Processor_core.append(contents[-3])
-                    Processor_GHz.append(np.nan)
-            else:
-                Processor_name.append(np.nan)
-                Processor_core.append(np.nan)
-                Processor_GHz.append(np.nan)
-
-        Processor_name = pd.Series(Processor_name)
-        Processor_core = pd.Series(Processor_core)
-        Processor_GHz = pd.Series(Processor_GHz)
-        return [Processor_name,Processor_core,Processor_GHz]
-
-
-    def initialize_data_processing(self, train_data_path, test_data_path):
+    def initialize_data_processing(self, raw_data_path):
         try:
             logging.info("Data Processing started")
 
-            # Let's load the training and testing data
-            train_data = pd.read_csv(train_data_path)
-            test_data = pd.read_csv(test_data_path)
-            logging.info("Training and testing data loaded")
+            # Let's first load the raw structurd data
+            df = pd.read_csv(raw_data_path)
 
-            # Let's first remove some unnecessary columns
-            train_data.drop(['os'],axis=1,inplace=True)
-            test_data.drop(['os'],axis=1,inplace=True)
-            logging.info("Removed unnecessary column")
+            # let's do train and test split and save the files
+            train_data,test_data = train_test_split(df,random_state=1,train_size=0.8)
 
-            # Removing rupee and , character from the 'price' feature and converting to int
-            for data in [train_data,test_data]:
-                for index in range(len(data)):
-                    item_price = data.loc[index, 'price']
-                    item_price = item_price.replace("₹", "").replace(",", "")
-                    item_price = re.sub(r'[^\d]', '', item_price)  # Remove any non-digit characters
-                    data.loc[index, 'price'] = item_price
+            # Let's make the directories to store the files
+            os.makedirs(os.path.dirname(self.paths.train_csv_path), exist_ok=True)
 
-                    # Let's create categories for rating
-                    item_rating = data.loc[index, 'rating']
-                    if item_rating > 80.0:
-                        data.loc[index, 'rating'] = '8+'
-                    elif item_rating > 70.0:
-                        data.loc[index, 'rating'] = '7+'
-                    elif item_rating > 60.0:
-                        data.loc[index, 'rating'] = '6+'
-
-                data['price'] = data['price'].astype(np.int64)
-            logging.info("Cleaned the price feature and created categories for rating feature")
+            # Let's now store the files
+            train_data.to_csv(self.paths.train_csv_path, index=False, header=True)
+            test_data.to_csv(self.paths.test_csv_path, index=False, header=True)
+            logging.info("Files saved")
 
 
-            #Extracting features from sim feature of training and testing data
-            for data in [train_data,test_data]:
-                list_series_sim = self.extract_encode_sim(data=data)
-                list_series_ram = self.extract_encode_ram(data=data)
-                list_series_battery = self.extract_encode_battery(data=data)
-                list_seires_display = self.extract_encode_display(data=data)
-                list_series_camera = self.extract_encode_camera(data=data)
-                list_processor_info = self.extract_processor_info(data = data)
-
-                data['Has_5g'] = list_series_sim[0]
-                data['Add_Features'] = list_series_sim[1]
-
-                data['RAM'] = list_series_ram[0]
-                data['Storage'] = list_series_ram[1]
-
-                data['Battery'] = list_series_battery[0]
-                data['Charging'] = list_series_battery[1]
-
-                data['Screen_RR'] = list_seires_display[0]
-                data['PPI'] = list_seires_display[1]
-
-                data['rear_cams'] = list_series_camera[0]
-                data['Total_fmp'] = list_series_camera[1]
-
-                data['Processor_name'] = list_processor_info[0]
-                data['Processor_core'] = list_processor_info[1]
-                data['Processor_GHz'] = list_processor_info[2]
-                # Let's fix some errors in the processor_name feature
-                data.loc[(data['Processor_name'] == "A13"), 'Processor_name'] = "Bionic"
-                data.loc[data['Processor_name'] == 'Sanpdragon', 'Processor_name'] = "Snapdragon"
-
-                # Let's now drop all the old features
-                for feature in ['sim','ram','display','camera','battery','processor']:
-                    data.drop([feature],axis =1,inplace=True)
-
-                # Remvoing any unrelated information from the card feature and extracting the name of smartphone brand
-                for i in range(len(data)):
-                    item_model = data.loc[i, 'model']
-                    data.loc[i, 'model'] = item_model.split()[0]
-
-                    item_card = data.loc[i, 'card']
-                    if pd.isna(item_card) == False and 'Memory' not in item_card.split():
-                        data.loc[i, 'card'] = np.nan
-
-                # Renaming the column and fixing some incorrect values in both model and card feature
-                data.rename(columns={'model': 'brand'}, inplace=True)
-                data.loc[data['brand'] == 'Oppo', 'brand'] = 'OPPO'
-                data.loc[data["card"] == "Memory Card Supported, upto 1000GB", "card"] = "Memory Card Supported, upto 1TB"
-
-                # Removing dummy phones and outliers from the features
-                data = data[(data['price'] > 4000) & (data['price'] < 400000)]
-                data = data[data['RAM'] < 20]
-
-            logging.info("Extracted new features from old feature and removed old features successfully")
-
-            # Column transformer for univariate imputation (Mode)
             # Column transformer for univariate imputation (Mode)
             Mode_impute = ColumnTransformer(transformers=[
-                ('Mode_imputation', SimpleImputer(strategy='most_frequent'), [5,6,9, 10, 11, 12, 13])
+                ('Mode_imputation', SimpleImputer(strategy='most_frequent'), [5, 6 ,9, 10, 11, 12, 13])
             ], remainder='passthrough')
 
             # Column transformer for the ordinal encoding
@@ -370,13 +100,12 @@ class Data_processing:
             logging.info("Created a pipeline successfully")
 
             # Seperating the dependent and independent variable
-            X_train = train_data.drop(['price','Battery','Processor_GHz'], axis=1)
+            X_train = train_data.drop(['price'], axis=1)
             y_train = train_data['price']
 
-            X_test = test_data.drop(['price','Battery','Processor_GHz'], axis=1)
+            X_test = test_data.drop(['price'], axis=1)
             y_test = test_data['price']
             logging.info("Created X_train,y_train and X_test,y_test successfully")
-
 
             #Let's now process the data
             X_train = pipe.fit_transform(X_train, y_train)
@@ -386,12 +115,12 @@ class Data_processing:
             y_train = np.array(y_train.values)
             y_test = np.array(y_test.values)
 
-            # Let's now save the pipeline
-            save_object(file_path=self.pipe_path.pipeline_path, obj=pipe)
+            #Let's now save the pipeline
+            save_object(file_path=self.paths.pipeline_path, obj=pipe)
             logging.info("Saved pipeilne object")
 
             logging.info("Data Processing completed")
-            return (X_train, y_train, X_test, y_test)
+            return (X_train, y_train, X_test, y_test,self.paths.pipeline_path)
 
         except Exception as e:
             raise CustomException(e, sys)
