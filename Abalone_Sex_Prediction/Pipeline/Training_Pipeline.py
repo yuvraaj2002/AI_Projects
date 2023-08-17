399a3e800eb477909c87785db37131ef425c4bdf
Yuvraj Singh <ys2002github@gmail.com>
2023-08-17 10:13:59 +0530

    Made changes in the training pipeline by adding model training step

--- Abalone_Sex_Prediction/Pipeline/Training_Pipeline.py
+++ Abalone_Sex_Prediction/Pipeline/Training_Pipeline.py
@@ -1,6 +1,6 @@
 from zenml.config import DockerSettings
 from zenml.integrations.constants import MLFLOW
-from zenml.pipelines import pipeline
+from zenml import pipeline
 from Src.Ingest_data import ingest_data
 from Src.Process_data import process_data
 from Src.Train_model import train_model
@@ -11,7 +11,7 @@ docker_settings = DockerSettings(required_integrations=[MLFLOW])
 @pipeline(
     name="train_pipeline", enable_cache=False, settings={"docker": docker_settings}
 )
-def train_pipeline(step_1, step_2, step_3):
+def train_pipeline():
     """
     Args:
         step_1: DataClass,
@@ -20,14 +20,10 @@ def train_pipeline(step_1, step_2, step_3):
     return:
         None
     """
-    df = step_1("Dataset.txt")
-    X_train, X_test, y_train, y_test = step_2(df)
-    model = step_3(X_train, X_test, y_train, y_test)
+    df = ingest_data("Dataset.txt")
+    X_train, X_test, y_train, y_test = process_data(df)
+    model = train_model(X_train, X_test, y_train, y_test)
 
 
 if __name__ == "__main__":
-    train_pipeline(
-        step_1=ingest_data(),
-        step_2=process_data(),
-        step_3=train_model()
-    ).run()
+    train_pipeline()
