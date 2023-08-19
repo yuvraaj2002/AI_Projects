9965e4898ed7e952d76bd14329b32371d2977655
Yuvraj Singh <ys2002github@gmail.com>
2023-08-19 20:15:25 +0530

    Deleted old files"

--- Smartphone_Price_Prediction/src/components/Model_config.py
+++
@@ -1,9 +0,0 @@
-from zenml.steps import BaseParameters
-import os
-
-class ModelNameConfig(BaseParameters):
-    """Model Configurations"""
-
-    model_name: str = "lightgbm"
-    fine_tuning: bool = False
-    mode_storage_path = os.path.join("Storage", "Model.pkl")
