20aeab2680f5386f02a85cb1736c5adaf91722b6
Yuvraj Singh <ys2002github@gmail.com>
2023-08-22 12:25:36 +0530

    Updated the model configuration file

--- Smartphone_Price_Prediction/Model_info/Model_config.py
+++ Smartphone_Price_Prediction/Model_info/Model_config.py
@@ -5,6 +5,6 @@ import os
 class ModelNameConfig(BaseParameters):
     """Model Configurations"""
 
-    model_name: str = "randomforest"
+    model_name: str = "lightgbm"
     fine_tuning: bool = False
     model_storage_path = os.path.join("Artifacts", "Model.pkl")
