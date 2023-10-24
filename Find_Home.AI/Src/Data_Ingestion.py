f3986115f63967c37b9a33c4622eb7bd8b1cab6c
Yuvraj Singh <ys2002github@gmail.com>
2023-10-24 13:49:40 +0530

    Updated the class name, member function name and return type of zenml step

--- Find_Home.AI/Src/Data_Ingestion.py
+++ Find_Home.AI/Src/Data_Ingestion.py
@@ -1,15 +1,19 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 from zenml import step
 from Src.Exception import CustomException
+from typing_extensions import Annotated
+from typing import Tuple
+from sklearn.preprocessing import OrdinalEncoder,OneHotEncoder,MinMaxScaler,FunctionTransformer
 from sklearn.model_selection import train_test_split
 
-class Data_Ingestion:
+class DIP:
     def __int__(self):
         pass
 
-    def data_ingestion(self, path):
+    def Data_Ingest_Process(self, path):
         """
         This method will take the file path as input and will load the data from the given path,
         perform train, test split and save the files in the specified directory
@@ -30,18 +34,19 @@ class Data_Ingestion:
             # Let's now store the files
             Train_df.to_csv(os.path.join("Artifacts", "Train.csv"), index=False, header=True)
             Test_df.to_csv(os.path.join("Artifacts", "Test.csv"), index=False, header=True)
-            return (Train_df,Test_df)
+
+            return Train_df,Test_df
 
         except Exception as e:
             raise CustomException(e, sys)
 
 @step
-def ingest_data(path: str) -> pd.DataFrame:
-    ingest_obj = Data_Ingestion()
-    Train_df,Test_df = ingest_obj.data_ingestion(path)
+def ingest_data(path: str) -> (Tuple[pd.DataFrame,pd.DataFrame]):
+    ingest_obj = DIP()
+    Train_df,Test_df = ingest_obj.Data_Ingest_Process(path)
     return Train_df,Test_df
 
-# USE THE BELOW MENTIONED CODE TO CHECK IF EVERYTHING IS WORKING FINE
+# USE THE BELOW-MENTIONED CODE TO CHECK IF EVERYTHING IS WORKING FINE
 # if __name__ == "__main__":
 #     ingest_obj = Data_Ingestion()
 #     Train_df, Test_df = ingest_obj.data_ingestion("/home/yuvraj/Github/Machine_Learning_Projects/Find_Home.AI/Notebook_And_Dataset/Cleaned_datasets/Combined_CleanData_V4.csv")
