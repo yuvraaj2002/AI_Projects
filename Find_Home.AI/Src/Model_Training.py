6694b8911f17cdfa1dce462ce2da83b314e44bd3
Yuvraj Singh <ys2002github@gmail.com>
2024-01-29 13:18:34 +0530

    Updated training file

--- Find_Home.AI/Src/Model_Training.py
+++ Find_Home.AI/Src/Model_Training.py
@@ -24,7 +24,6 @@ class Model_training_Class:
 
     def initialize_model_training(self, X_train, X_test, y_train, y_test):
         try:
-
             model = None
             tuner = None
 
@@ -43,32 +42,41 @@ class Model_training_Class:
 
             # If fine_tuning is set to be true in Model configuration file
             if model_config_obj.fine_tuning == True:
-                find_hparms_obj = HyperparameterTuner(X_train,X_test,y_train,y_test)
-                hyper_parms = find_hparms_obj.optimize() # Getting best set of hyper-parameters
+                find_hparms_obj = HyperparameterTuner(X_train, X_test, y_train, y_test)
+                hyper_parms = (
+                    find_hparms_obj.optimize()
+                )  # Getting best set of hyper-parameters
 
                 # Create the XGBoost Regressor with the best hyperparameters
                 Tuned_model = XGBRegressor(
-                    max_depth=int(hyper_parms['max_depth']),
-                    gamma=hyper_parms['gamma'],
-                    reg_alpha=hyper_parms['reg_alpha'],
-                    reg_lambda=hyper_parms['reg_lambda'],
-                    colsample_bytree=hyper_parms['colsample_bytree'],
-                    min_child_weight=hyper_parms['min_child_weight'],
+                    max_depth=int(hyper_parms["max_depth"]),
+                    gamma=hyper_parms["gamma"],
+                    reg_alpha=hyper_parms["reg_alpha"],
+                    reg_lambda=hyper_parms["reg_lambda"],
+                    colsample_bytree=hyper_parms["colsample_bytree"],
+                    min_child_weight=hyper_parms["min_child_weight"],
                     n_estimators=180,
-                    random_state=0)
+                    random_state=0,
+                )
 
                 # Training the model
                 Tuned_model.fit(X_train, y_train)
 
                 # Let's save the trained model
-                save_object(file_path=model_config_obj.model_storage_path, obj=Tuned_model)
+                save_object(
+                    file_path=model_config_obj.model_storage_path, obj=Tuned_model
+                )
                 return Tuned_model
 
             else:
-                Without_Tuning_model = model.fit(X_train, y_train)  # Train model with base params
-                save_object(file_path=model_config_obj.model_storage_path, obj=Without_Tuning_model)
+                Without_Tuning_model = model.fit(
+                    X_train, y_train
+                )  # Train model with base params
+                save_object(
+                    file_path=model_config_obj.model_storage_path,
+                    obj=Without_Tuning_model,
+                )
                 return Without_Tuning_model
-
 
         except Exception as e:
             raise CustomException(e, sys)
@@ -77,10 +85,8 @@ class Model_training_Class:
 # @step(experiment_tracker=experiment_tracker.name)
 @step
 def train_model(
-    X_train: np.ndarray,
-    X_test: np.ndarray,
-    y_train: np.ndarray,
-    y_test: np.ndarray) -> RegressorMixin:
+    X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray
+) -> RegressorMixin:
     """
     Args:
         X_train: np.array,
@@ -91,5 +97,7 @@ def train_model(
         model: RegressorMixin
     """
     model_train_obj = Model_training_Class()
-    trained_model = model_train_obj.initialize_model_training(X_train, X_test, y_train, y_test)
+    trained_model = model_train_obj.initialize_model_training(
+        X_train, X_test, y_train, y_test
+    )
     return trained_model
