d5b317b02f107bd3ae5452d5f22bc1ef844bdf25
Yuvraj Singh <ys2002github@gmail.com>
2024-03-08 11:01:47 +0530

    Added the lemmatization in the clean_data function

--- AI_Tutor/streamlit/pages/3_Resume_Radar.py
+++ AI_Tutor/streamlit/pages/3_Resume_Radar.py
@@ -4,9 +4,12 @@ from io import BytesIO
 from PyPDF2 import PdfReader
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import CTransformers
+from nltk.stem import WordNetLemmatizer
 import io
 import string
 import nltk
+import numpy as np
+from nltk.corpus import wordnet
 import re
 
 st.markdown(
@@ -52,7 +55,7 @@ def llama_response(Input_text):
         input_variables=["data"],
         template="""
         Given a candidate's profile stored in the provided {data} text, automatically extract information about their projects, experience, and achievements.
-        Utilize this extracted information to craft 10 interview questions that an interviewer might ask during the hiring process."""
+        Utilize this extracted information to craft 10 interview questions that an interviewer might ask during the hiring process.""",
     )
 
     model_response = llm_model(Que_template.format(data=Input_text))
@@ -61,25 +64,27 @@ def llama_response(Input_text):
 
 def clean_data(text):
     """
-        This function preprocesses text by performing the following steps:
+    This function preprocesses text by performing the following steps:
 
-        1. **Lowercasing:** Converts all characters to lowercase.
-        2. **Tokenization:** Splits the text into individual words using NLTK word tokenization.
-        3. **Stopword removal:** Removes common stop words (e.g., "the", "a", "is") from the word list.
-        4. **Punctuation removal:** Removes punctuation marks from the word list.
-        5. **Remove phone numbers and email addresses:** Removes phone numbers and email addresses from the text.
-        6. **Rejoining:** Joins the remaining words back into a string with spaces in between.
+    1. **Lowercasing:** Converts all characters to lowercase.
+    2. **Tokenization:** Splits the text into individual words using NLTK word tokenization.
+    3. **Stopword removal:** Removes common stop words (e.g., "the", "a", "is") from the word list.
+    4. **Punctuation removal:** Removes punctuation marks from the word list.
+    5. **Remove phone numbers and email addresses:** Removes phone numbers and email addresses from the text.
+    6. **Lemmatization:** Lemmatizes words to their base form.
+    7. **Rejoining:** Joins the remaining words back into a string with spaces in between.
 
-        Args:
-            pdf_data (bytes): The PDF data to be processed.
+    Args:
+        text (str): The text to be cleaned.
 
-        Returns:
-            str: The cleaned text with stop words, punctuation, phone numbers, and email addresses removed.
-        """
-
+    Returns:
+        str: The cleaned text with stop words, punctuation, phone numbers, email addresses, and lemmatization applied.
+    """
+    # Lowercase the text
     text_lower = text.lower()
+
+    # Tokenize the text
     tokens = nltk.word_tokenize(text_lower)
-    stop_words = set(nltk.corpus.stopwords.words("english"))
 
     # Remove punctuation marks
     tokens = [word for word in tokens if word not in string.punctuation]
@@ -87,7 +92,7 @@ def clean_data(text):
     # Remove phone numbers
     cleaned_tokens = []
     for token in tokens:
-        if not re.match(r'\+?\d[\d -]{8,12}\d', token):
+        if not re.match(r"\+?\d[\d -]{8,12}\d", token):
             cleaned_tokens.append(token)
 
     # Remove "gmail.com" after punctuation removal
@@ -96,11 +101,34 @@ def clean_data(text):
         if not token.endswith("gmail.com"):
             final_tokens.append(token)
 
-    filtered_text = " ".join(final_tokens)
-    return filtered_text
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    lemma_tokens = [lemmatizer.lemmatize(token, wordnet.VERB) for token in final_tokens]
+
+    # Rejoin the tokens into a string
+    cleaned_text = " ".join(lemma_tokens)
+    return cleaned_text
+
+
+# Define a function to get the vector representation of a document using Word2Vec
+# def document_vector(doc):
+#     # Remove out-of-vocabulary words and get Word2Vec vectors for the words in the document
+#     words = [word for word in doc.split() if word in W2V_model]
+#     if not words:
+#         # If none of the words are in the Word2Vec model, return zeros
+#         return np.zeros(300)
+#
+#     # Return the mean of Word2Vec vectors for words in the document
+#     return np.mean(W2V_model[words], axis=0)
+#
+#
+# # Apply the function to each document in Facilities_df['Facilities_Str']
+# word2vec_matrix = np.array(
+#     [document_vector(doc) for doc in Facilities_df["Facilities_Str"]]
+# )
+
 
 def process_clean(pdf_data):
-
 
     file_object = io.BytesIO(pdf_data)  # Create a BytesIO object
     reader = PdfReader(file_object)
@@ -111,6 +139,7 @@ def process_clean(pdf_data):
         text += page.extract_text()
 
     return clean_data(text)
+
 
 def resume_radar_page():
 
@@ -131,16 +160,16 @@ def resume_radar_page():
 
         if uploaded_file is not None:
             pdf_data = uploaded_file.read()
-            b64_pdf = base64.b64encode(pdf_data).decode('utf-8')
+            b64_pdf = base64.b64encode(pdf_data).decode("utf-8")
             pdf_display = f'<embed src="data:application/pdf;base64,{b64_pdf}" width="690" height="740" type="application/pdf">'
             st.markdown(pdf_display, unsafe_allow_html=True)
 
             st.write("")
-            analyze_bt = st.button("Analyze my resume🔎",use_container_width=True)
+            analyze_bt = st.button("Analyze my resume🔎", use_container_width=True)
             if analyze_bt:
                 with col1:
                     st.write(clean_data(job_description))
-                    #st.write(process_clean(pdf_data))
+                    # st.write(process_clean(pdf_data))
 
 
 resume_radar_page()
